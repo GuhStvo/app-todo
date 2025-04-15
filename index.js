0e7bd94568970440e249e82c7f9a1ace67364cae
GuhStvo <gustavo.asilva86@senacsp.edu.br>
2025-04-14 22:10:02 -0300

    Aplicativo Nativo

---
+++ index.js
@@ -0,0 +1,5 @@
+import { registerRootComponent } from "expo";
+
+import App from './App';
+
+registerRootComponent(App); // Registrando o componente principal, para o react saber qual é o arquivo principal
